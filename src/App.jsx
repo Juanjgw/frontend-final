87ff15fb56239084f12ceb16179aef094921aa24
Juanjgw <juanjgw@gmail.com>
2024-07-07 19:25:18 -0300

    primer commit de homescreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import RouterApp from './RouterApp'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
--- src/screens/Home/HomeScreen.jsx
+++ src/screens/Home/HomeScreen.jsx
@@ -1,23 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ServiceList from '../Services/ServiceList'; 
+
 
 const HomeScreen = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    // Borrar el token de localStorage
     localStorage.removeItem('token');
-    
-    // Redirigir al usuario a la página de inicio de sesión
     navigate('/login');
   };
 
+  const handleContact = () => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      navigate('/login');
+    } else {
+      // Lógica para contactar con el proveedor de servicios
+    }
+  };
+
+  // Datos de prueba para servicios
+  const services = [
+    { id: 1, title: 'Servicio 1', description: 'Descripción del servicio 1.', rating: 4 },
+    { id: 2, title: 'Servicio 2', description: 'Descripción del servicio 2.', rating: 4 },
+    { id: 3, title: 'Servicio 3', description: 'Descripción del servicio 3.', rating: 4 },
+    { id: 4, title: 'Servicio 4', description: 'Descripción del servicio 4.', rating: 4 },
+    { id: 5, title: 'Servicio 5', description: 'Descripción del servicio 5.', rating: 4 },
+  ];
+
+  const filteredServices = services.filter(service =>
+    service.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div>
-      <h1>Bienvenido!</h1>
-      <button onClick={handleLogout}>Cerrar sesión</button>
+    <div className="container">
+      <div className="d-flex justify-content-between align-items-center my-4">
+        <h1>Bienvenido!</h1>
+        <button className="btn btn-danger" onClick={handleLogout}>Cerrar sesión</button>
+      </div>
+      <div className="row search-bar mb-4">
+        <div className="col-md-12">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Buscar servicios..."
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+          />
+        </div>
+      </div>
+      <ServiceList services={filteredServices} handleContact={handleContact} />
     </div>
   );
-};
+}
 
 export default HomeScreen;
