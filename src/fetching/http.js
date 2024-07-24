4fe85e4f151a8ab5083f5179a1b9aa3b121a07ce
Juanjgw <juanjgw@gmail.com>
2024-07-24 10:13:31 -0300

    cambie url_api de produccion

--- src/fetching/http.js
+++ src/fetching/http.js
@@ -31,8 +31,9 @@ export const URL = {
    // tp final 
    // URL_API: 'https://backend-final-con-sql.onrender.com', 
    // local
-   URL_API: 'http://localhost:4041',
-   // produccion
+   // URL_API: 'http://localhost:4041',
+      // produccion
    // URL_API: 'https://contrata-expertos-backed.onrender.com', 
+   URL_API: 'https://contrataexpertos.com',
 
 }
