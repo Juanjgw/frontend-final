446d7a44c1983e664733fbc52c9c6dd48a84cf99
Juanjgw <juanjgw@gmail.com>
2024-08-01 15:03:03 -0300

    http para produccion

--- src/fetching/http.js
+++ src/fetching/http.js
@@ -54,10 +54,10 @@ export const URL = {
    // tp final 
    // URL_API: 'https://backend-final-con-sql.onrender.com', 
    // local
-    URL_API: 'http://localhost:4041',
+   // URL_API: 'http://localhost:4041',
  // produccion
-   //URL_API: 'https://backend-contrata-expertos.onrender.com', 
+   URL_API: 'https://backend-contrata-expertos.onrender.com', 
   
-   //URL_API: 'https://contrataexpertos.com',
+  // URL_API: 'https://contrataexpertos.com',
 
 }
